530b8705dc7f45f621fc3def2660d632f88694c7
sammrs <mirsiansaeed@gmail.com>
2025-03-07 20:51:06 +0330

    Added program to create list with user input numbers

--- py/22.py
+++ py/22.py
@@ -1,6 +1,11 @@
-a = int(input())
-c = []
+# Get the size of list
+a = int(input("Enter how many numbers you want: "))
+c = []  # Empty list to store numbers
+
+# Get numbers from user
 for i in range(a):
-    c.append(c[i])
-print(c)
+    num = int(input(f"Enter number {i+1}: "))
+    c.append(num)  # Add the number to list
+
+print("Your list:", c)
 
